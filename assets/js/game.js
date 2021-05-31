de03ebfe89a9130cc63d90a09960cb8cc1a9c1c7
Yussef Rafat <yussefrafat5908@gmail.com>
2021-05-31 12:42:23 -0400

    testing JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight" 
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName window.alert(playerName)
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+
+console.log(10+10);
+// whta is this?
+console.log("Our robot's name is" + playerName);
+
+
