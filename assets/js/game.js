827de4ba936157bdcf744990c230566a0fdd99a2
Yussef Rafat <yussefrafat5908@gmail.com>
2021-06-04 12:48:56 -0400

    Player Name Prompt Error Handling

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,10 +168,22 @@ var randomNumber = function(min, max) {
   /* END GAME FUNCTIONS */
   
   /* GAME INFORMATION / VARIABLES */
+  var getPlayerName = function() {
+      var name = "";
   
+  // ****************************************
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  while (name === "" || name === null) {
+      name = prompt("What is your robot's name?");
+  }
+  // ***************************************
+      console.log("Your robot's name is " + name);
+      return name;
+  };
+
   // player information
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
